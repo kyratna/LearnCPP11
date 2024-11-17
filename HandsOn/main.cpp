7d19e541b61cf1b6f84b8d0c38069c8d7fc155b5
kyratna <42309574+kyratna@users.noreply.github.com>
2024-11-17 14:40:56 +0530

    updated memory leak demo cpp

--- HandsOn/main.cpp
+++ HandsOn/main.cpp
@@ -7,12 +7,13 @@ using namespace std;
 int func() {
     for(int i = 0; i < 100000; i++) {
         cout << "Hey\n";
-        int* a = new int(10); // dynamic memory allocation on heap.
+        int* a = new int(10); // dynamic memory allocation which is on heap.
     }
 }
 
 int main() {
-    func(); //repeatingly calling dynamic memory allocation consuming the memory as program flow move furthur.
+    func(); // calling func which is using dynamic memory allocation
+            // consuming the memory as program control move furthur with each iteration.
     
     
     return 0;
