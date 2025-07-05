764f9d89dec820e7a40d3a534739117dfd316fc9
kyratna <42309574+kyratna@users.noreply.github.com>
2025-07-05 12:51:50 +0530

    pass by value / address / reference

--- Notes/ReferencePointers/ReferencePointersSwap.cpp
+++ Notes/ReferencePointers/ReferencePointersSwap.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void Swap(int x, int y) {
+// pass by value
+// a copy is created of the variables
+// actual variables are never swapped
+void SwapByValue(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+// pass by address
+// copy is not created of variables
+// actual variables values are swapped
+void SwapByAddress(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// pass by Reference
+// copy is not created of variables
+// actual variables values are swapped
+void SwapByReference(int &x, int &y) {
     int temp = x;
     x = y;
     y = temp;
@@ -10,7 +31,18 @@ void Swap(int x, int y) {
 int main() {
     int a = 5, b = 10;
     cout << "a: " << a << " " << "b: " << b << endl;
-    Swap(a, b);
+    SwapByValue(a, b);
     cout << "a: " << a << " " << "b: " << b << endl;
+
+    int c = 15, d = 20;
+    cout << "c: " << c << " " << "d: " << d << endl;
+    SwapByAddress(&c, &d);
+    cout << "c: " << c << " " << "d: " << d << endl;
+
+    int e = 25, f = 30;
+    cout << "e: " << e << " " << "f: " << f << endl;
+    SwapByReference(e, f);
+    cout << "e: " << e << " " << "f: " << f << endl;
+
     return 0;
 }
