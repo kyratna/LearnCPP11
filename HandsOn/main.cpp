ae87b8b4a0bdbae6450ebc12653bad3e68cdab43
kyratna <42309574+kyratna@users.noreply.github.com>
2025-07-06 11:11:26 +0530

    const pointer and const variable

--- HandsOn/main.cpp
+++ HandsOn/main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 using namespace std;
 
-void Swap(int x, int y) {
-    int temp = x;
-    x = y;
-    y = temp;
-}
 
 int main() {
-    int a = 5, b = 10;
-    cout << "a: " << a << " " << "b: " << b << endl;
-    Swap(a, b);
-    cout << "a: " << a << " " << "b: " << b << endl;
+    int x = 10;
+    int *ptr = &x;
+    cout << x << " " << *ptr << endl;
+    *ptr = 20;
+    cout << x << " " << *ptr << endl;
+
+    // #######################################################//
+
+    // y is a const int
+    // y is read only type, writing it is compile time error
+    const int y = 10;
+    // ptr2 is a pointer to an integer (which is non const)
+    // ptr2 do not qualify to store address of const int y
+    // int *ptr2 = &y; // error
+    // *ptr2 = 20; // error
+
+    // #######################################################//
+
+    // z is a const int
+    // z is read only type, writing it is compile time error
+    const int z = 10;
+    // ptr3 is a pointer to a const int
+    // ptr3 qualifies to keep address of const int z
+    const int *ptr3 = &z;
+    // ptr3 can not be used to modify the const int z
+    // *ptr3 = 20; // error
+
+    // #######################################################//
+
+    const int t = 10;
+    // const ptr4 pointing to const int
+    const int *const ptr4 = &t;
+    // ptr4 = &y; // error, 2nd const, ptr4 is a constant pointer, can’t be reassigned
+    // *ptr4 = 20; // error, 1st const, the pointee is const int, can’t be modified
+
     return 0;
 }
